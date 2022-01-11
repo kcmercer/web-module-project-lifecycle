9ffcdecfa51961fb579b4f0ff6ae71b00fba708b
Kenneth Mercer <kmercer5439@gmail.com>
2022-01-11 13:13:20 -0600

    built basic user holder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import User from './components/User';
+
+
 
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
+      <h1>GITHUB INFO</h1>
+      <form>
+        <input
+          type="text"
+          name="search"
+          />
+        <button> Search </button>
+      </form>
+
+      <User />
+
+      <div>
+        <h2> Followers: </h2>
+        <div>
+          <img width="200" src="https://www.facebeautyscience.com/wp-content/uploads/2020/04/face-beauty-skin-face2-proc.jpg" />
+          <h4> ladyBeard </h4>
+        </div>
+      </div>
     </div>);
   }
 }
---
+++ src/components/User.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class User extends React.Component {
+    state = {
+        avatar_url:"",
+        name:"",
+        login:"",
+        public_repos:0,
+        followers:0
+    }
+
+    componentDidMount() {
+        axios.get("https://api.github.com/users/kcmercer")
+            .then(resp => {
+                console.log(resp.data)
+                this.setState({
+                    avatar_url: resp.data.avatar_url,
+                    name: resp.data.name,
+                    login: resp.data.login,
+                    public_repos: resp.data.public_repos,
+                    followers: resp.data.followers 
+                })
+                console.log(this.state);
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                <img width="300" src={this.state.avatar_url} />
+                <h2> {this.state.name} </h2>
+                <h4> {this.state.login} </h4>
+                <h3> Public Repos - {this.state.public_repos} </h3>
+                <h3> Followers - {this.state.followers} </h3>
+            </div>
+        )}
+}
+
+export default User;
